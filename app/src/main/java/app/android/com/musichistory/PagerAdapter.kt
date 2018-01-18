f98ac1860d4c9fdf9ecdf01dce1e4c9a7bbd9094
akash bisariya <bigakash@gmail.com>
2018-01-19 00:32:30 +0530

    Added view pager

---
+++ app/src/main/java/app/android/com/musichistory/PagerAdapter.kt
@@ -0,0 +1,17 @@
+package app.android.com.musichistory
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentStatePagerAdapter
+
+/**
+ * Created by akash bisariya on 18-01-2018.
+ */
+class PagerAdapter : FragmentStatePagerAdapter() {
+    override fun getItem(position: Int): Fragment {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
